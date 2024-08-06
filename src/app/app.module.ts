938450e2b34b261a3fd424ce54ab058ea08c818e
sritheja312 <sritheja.pothooru@antra.com>
2024-08-06 13:55:19 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlooringComponent } from './components/flooring/flooring.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlooringComponent
   ],
   imports: [
     BrowserModule,
