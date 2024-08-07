0fb87b5694d2d1ac8288455f75228a47a07769bc
nuthalapati000121 <84333197+nuthalapati000121@users.noreply.github.com>
2024-08-07 18:04:51 +0530

    changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,43 +12,6 @@ import { DropDownAnimation } from './drop-animation';
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  // animations: [
-  //   trigger('hamburguerX', [
-  //     /*
-  //       state hamburguer => is the regular 3 lines style.
-  //       states topX, hide, and bottomX => used to style the X element
-  //     */
-  //     state('hamburguer', style({})),
-  //     // style top bar to create the X
-  //     state(
-  //       'topX',
-  //       style({
-  //         transform: 'rotate(45deg)',
-  //         transformOrigin: 'left',
-  //         margin: '6px',
-  //       })
-  //     ),
-  //     // hides element when create the X (used in the middle bar)
-  //     state(
-  //       'hide',
-  //       style({
-  //         opacity: 0,
-  //       })
-  //     ),
-  //     // style bottom bar to create the X
-  //     state(
-  //       'bottomX',
-  //       style({
-  //         transform: 'rotate(-45deg)',
-  //         transformOrigin: 'left',
-  //         margin: '6px',
-  //       })
-  //     ),
-  //     transition('* => *', [
-  //       animate('0.2s'), // controls animation speed
-  //     ]),
-  //   ]),
-  // ],
   animations: [DropDownAnimation],
 })
 export class HeaderComponent implements OnInit, OnChanges {
@@ -56,6 +19,7 @@ export class HeaderComponent implements OnInit, OnChanges {
   open = false;
   isHamburguer = true;
   menuState: string = 'out';
+  openSolutions = false;
   ngOnInit(): void {
     console.log('window.innerWidth', window.innerWidth);
   }
@@ -68,5 +32,8 @@ export class HeaderComponent implements OnInit, OnChanges {
   }
   toggleMenu() {
     this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  }
+  mouseEnter(data:any) {
+    this.openSolutions = true;
   }
 }
