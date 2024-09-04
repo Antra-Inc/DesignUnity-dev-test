e6a2f0d2553cc21b987d6680654e147a71f5a465
sritheja312 <sritheja.pothooru@antra.com>
2024-09-04 17:10:46 +0530

    blog-list changes

--- src/app/components/blog-list/blog-list.component.ts
+++ src/app/components/blog-list/blog-list.component.ts
@@ -36,100 +36,22 @@ export class BlogListComponent implements OnInit {
   }
 
 
-
-  items = [
-    {
-      category: 'Venetian Plasters',
-      img: '../../../assets/img/flooring/banner.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/5.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/4.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Venetian Plasters',
-      img: '../../../assets/img/flooring/1.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/2.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/6.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Venetian Plasters',
-      img: '../../../assets/img/flooring/banner.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/5.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/4.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Venetian Plasters',
-      img: '../../../assets/img/flooring/1.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/2.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    },
-    {
-      category: 'Flooring',
-      img: '../../../assets/img/flooring/6.png',
-      heading: 'Lorem ipsum dolor sit amet consectetur adipisicing elit, sed',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptatum obcaecati odit provident fuga, nemo porro illum laboriosam'
-    }
-  ]
-
-
-  
-  filteredCards = this.items; // Initially show all cards
-  visibleCardsCount: number = 6; // Show 3 items initially
+  filteredCards = this.posts; // Initially show all cards
+  visibleCardsCount: number = 3; // Show 3 items initially
 
   // Method to filter cards by category
   filterByCategory(category: string) {
     if (category === 'all') {
-      this.filteredCards = this.items;
+      this.filteredCards = this.posts;
     } else {
-      this.filteredCards = this.items.filter(item => item.category === category);
+      this.filteredCards = this.posts.filter(item => item.category === category);
     }
-    this.visibleCardsCount = 6; // Reset visible cards to 3 when a category is clicked
+    this.visibleCardsCount = 3; // Reset visible cards to 3 when a category is clicked
   }
 
   // Method to load more cards
   loadMore() {
-    this.visibleCardsCount += 3; // Show 4 more cards when load more is clicked
+    this.visibleCardsCount += 3; // Show 3 more cards when load more is clicked
   }
 
 
