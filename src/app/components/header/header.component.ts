b7c641599647209333361d9d5ba42553731d791d
sritheja312 <sritheja.pothooru@antra.com>
2024-09-16 21:50:34 +0530

    changes in styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,7 @@ import {
 import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { DropDownAnimation } from './drop-animation';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -22,6 +23,8 @@ export class HeaderComponent implements OnInit, OnChanges {
   openSolutions = false;
   imgSrc = '';
   activeItem = '';
+  submenuOpen = false; 
+
   ngOnInit(): void {
     console.log('window.innerWidth', window.innerWidth);
   }
@@ -32,9 +35,9 @@ export class HeaderComponent implements OnInit, OnChanges {
   onClick() {
     this.isHamburguer = !this.isHamburguer;
   }
-  toggleMenu() {
-    this.menuState = this.menuState === 'out' ? 'in' : 'out';
-  }
+  // toggleMenu() {
+  //   this.menuState = this.menuState === 'out' ? 'in' : 'out';
+  // }
   mouseEnter(data: any) {
     this.openSolutions = true;
   }
@@ -42,6 +45,22 @@ export class HeaderComponent implements OnInit, OnChanges {
     this.activeItem = data;
   }
 
+toggleSubMenu() {
+  this.submenuOpen = !this.submenuOpen;
+}
 
-  
+// Toggle the main mobile menu
+toggleMenu() {
+  this.open = !this.open;
+  // When closing the menu, also close any open submenus
+  if (!this.open) {
+    this.submenuOpen = false;
+  }
+}
+
+navigateAndClose() {
+  this.open = false;
+  this.submenuOpen = false;
+}
+
 }
