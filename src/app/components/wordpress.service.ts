d956de24dcfcf3d254390329218bdf917c720153
sritheja312 <sritheja.pothooru@antra.com>
2024-11-22 13:17:10 +0530

    Update wordpress.service.ts

--- src/app/components/wordpress.service.ts
+++ src/app/components/wordpress.service.ts
@@ -9,7 +9,7 @@ export class WordpressService {
 
 
 
-    private apiUrl = 'http://localhost/du_blog_post/wp-json/wp/v2';
+    private apiUrl = 'http://stagingsite.designunity.com/wp-admin';
     private postsUrl = `${this.apiUrl}/posts`;
     private categoriesUrl = `${this.apiUrl}/categories`;
 
