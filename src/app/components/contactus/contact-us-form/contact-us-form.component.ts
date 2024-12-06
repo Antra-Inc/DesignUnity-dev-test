72aafb871a665d90779d3b74e6cdcd5265f74ac3
nuthalapati000121 <84333197+nuthalapati000121@users.noreply.github.com>
2024-12-06 20:25:19 +0530

    changes in contact us

--- src/app/components/contactus/contact-us-form/contact-us-form.component.ts
+++ src/app/components/contactus/contact-us-form/contact-us-form.component.ts
@@ -1,11 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us-form',
   templateUrl: './contact-us-form.component.html',
-  styleUrls: ['./contact-us-form.component.scss']
+  styleUrls: ['./contact-us-form.component.scss'],
 })
 export class ContactUsFormComponent {
   constructor(private http: HttpClient) {}
@@ -14,14 +14,26 @@ export class ContactUsFormComponent {
     if (form.valid) {
       // If the form is valid, proceed with HTTP POST to submit data.
       const formData = new FormData();
-
-      // Iterate over the form values and append to formData
-      Object.keys(form.value).forEach(key => {
+      Object.keys(form.value).forEach((key) => {
         formData.append(key, form.value[key]);
       });
+      const headers = new HttpHeaders({
+        'Access-Control-Allow-Origin': 'Origin', // Replace with your actual header name and value
+        'Access-Control-Expose-Headers': 'Access-Control-Request-Method', // Example of an authorization header, if needed
+        'Access-Control-Max-Age': 'Access-Control-Request-Headers',
+        'Access-Control-Allow-Credentials': '',
+        'Access-Control-Allow-Methods': '',
+        'Access-Control-Allow-Headers': '',
+      });
+      // Iterate over the form values and append to formData
 
       // Send POST request to Zoho Form (or another server endpoint)
-      this.http.post('https://forms.zohopublic.in/techwish/form/ContactUs/formperma/j7K5rPV3EvnRbXIAr2aIx5-nM9QMyrvskyzPJTzY9Us/htmlRecords/submit', formData)
+      this.http
+        .post(
+          'https://forms.zohopublic.in/techwish/form/ContactUs/formperma/j7K5rPV3EvnRbXIAr2aIx5-nM9QMyrvskyzPJTzY9Us/htmlRecords/submit',
+          formData,
+          { headers }
+        )
         .subscribe({
           next: (response) => {
             console.log('Form submission successful', response);
