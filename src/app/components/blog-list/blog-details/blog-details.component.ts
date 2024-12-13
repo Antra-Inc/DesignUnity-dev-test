501da0eda5bfadd5aa8b9f8f8b1dd3d9509472d5
nuthalapati000121 <84333197+nuthalapati000121@users.noreply.github.com>
2024-12-13 21:31:13 +0530

    alignment changes

--- src/app/components/blog-list/blog-details/blog-details.component.ts
+++ src/app/components/blog-list/blog-details/blog-details.component.ts
@@ -5,28 +5,30 @@ import { WordpressService } from '../../wordpress.service';
 @Component({
   selector: 'app-blog-details',
   templateUrl: './blog-details.component.html',
-  styleUrls: ['./blog-details.component.scss']
+  styleUrls: ['./blog-details.component.scss'],
 })
-export class BlogDetailsComponent implements OnInit{
-post: any;
-  isLoading = true;  // Optional loading state
+export class BlogDetailsComponent implements OnInit {
+  post: any;
+  isLoading = true;
 
-  constructor(private route: ActivatedRoute, private postService: WordpressService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private postService: WordpressService
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    console.log('Post ID:', id);  // Check the post ID
+    console.log('Post ID:', id);
     if (id) {
       this.postService.getPostById(id).subscribe({
         next: (data: any) => {
-          console.log('Post data:', data);  // Log the response to check
+          console.log('Post data:', data);
           this.post = data;
         },
         error: (error) => {
           console.error('Error fetching post:', error);
-        }
+        },
       });
     }
   }
 }
-
